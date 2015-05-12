1a29d125a33a5880e887fbeefb2f17c131775fb1
thekevster08 <kevin.collins@outlook.com>
2015-05-12 12:04:48 -0400

    working recordJSONScript

--- recordJSONScript.py
+++ recordJSONScript.py
@@ -8,10 +8,10 @@ import time
 while True:
 	with contextlib.closing(sqlite3.connect('temperatures.db')) as database:
 		with contextlib.closing(database.cursor()) as cursor:
-			cursor.execute('select strftime("%s", timestamp)*1000, temp from temps')
+			cursor.execute('select strftime("%s", timestamp)*1000, chamberTemp, wortTemp, motorpv from temps')
 			temperatures = []
-			for timestamp, temp in cursor:
-				temperatures.append([timestamp, temp])
+			for timestamp, chamberTemp, wortTemp, motorpv in cursor:
+				temperatures.append([timestamp, chamberTemp, wortTemp, motorpv])
 
 	with open('temperatures.json','w') as outfile:
 		json.dump(temperatures, outfile)
