84ccb0ad94e91215edc45d75a98817cd63aec2ab
thekevster08 <kevin.collins@outlook.com>
2016-02-13 20:35:11 +0000

    tryin gagain

--- TemperatureControlTools.py
+++ TemperatureControlTools.py
@@ -5,7 +5,7 @@ HEATER_PIN = 22
 def heatOn():
     GPIO.setmode(GPIO.BCM) 
     GPIO.setup(HEATER_PIN, GPIO.OUT)
-    GPIO.output(HEATER_PIN, 1)
+    GPIO.output(HEATER_PIN, 1) 
 
 def heatOff():
     GPIO.setmode(GPIO.BCM) 
