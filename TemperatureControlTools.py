18c2dc5c4b03e4e066b59883648eb45d34756e83
thekevster08 <kevin.collins@outlook.com>
2016-02-13 20:33:36 +0000

    added file to correct directory

---
+++ TemperatureControlTools.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+HEATER_PIN = 22
+
+def heatOn():
+    GPIO.setmode(GPIO.BCM) 
+    GPIO.setup(HEATER_PIN, GPIO.OUT)
+    GPIO.output(HEATER_PIN, 1)
+
+def heatOff():
+    GPIO.setmode(GPIO.BCM) 
+    GPIO.setup(HEATER_PIN, GPIO.OUT)
+    GPIO.output(HEATER_PIN, 0)
